97b0303a7d70ecee4b2c504d4a40ffe1368af998
DennisParulianPanjaitan <drekodraco1@gmail.com>
2024-02-28 18:01:46 +0700

    Perubahan Langkah Langkah Praktikum Jobsheet 2

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+    // Uses first & second middleware...
 });
+Route::get('/user/profile', function () {
+ // Uses first & second middleware...
+ });
+});
+
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+    //
+    });
+   });
+   Route::middleware('auth')->group(function () {
+   Route::get('/user', [UserController::class, 'index']);
+   Route::get('/post', [PostController::class, 'index']);
+   Route::get('/event', [EventController::class, 'index']);
+   });
+
+   Route::prefix('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+   });
+
+   Route::redirect('/here', '/there');
+
+   Route::view('/welcome', 'welcome');
+   Route::view('/welcome', 'welcome', ['name' =>'Taylor']);
+   
